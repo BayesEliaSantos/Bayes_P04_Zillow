843df58596a92c6177f977e417e746e0544cb1f4
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-11-06 10:23:33 -0600

    Updated wrangle_zillow to reduce records for MVP model

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -6,78 +6,79 @@ from dfo import DFO
 
 sql = '''
 select 
-    p.`id`,
+--    p.`id`,
     p.`parcelid`,
-    p.`airconditioningtypeid`,
-    act.`airconditioningdesc`,
-    p.`architecturalstyletypeid`,
-    ast.`architecturalstyledesc`,
-    p.`basementsqft`,
+--    p.`airconditioningtypeid`,
+--    act.`airconditioningdesc`,
+--    p.`architecturalstyletypeid`,
+--    ast.`architecturalstyledesc`,
+--    p.`basementsqft`,
     p.`bathroomcnt`,
     p.`bedroomcnt`,
-    p.`buildingclasstypeid`,
-    bct.`buildingclassdesc`,
-    p.`buildingqualitytypeid`,
+--    p.`buildingclasstypeid`,
+--    bct.`buildingclassdesc`,
+--    p.`buildingqualitytypeid`,
     p.`calculatedbathnbr`,
     p.`calculatedfinishedsquarefeet`,
-    p.`decktypeid`,
-    p.`finishedfloor1squarefeet`,
-    p.`finishedsquarefeet12`,
-    p.`finishedsquarefeet13`,
-    p.`finishedsquarefeet15`,
-    p.`finishedsquarefeet50`,
-    p.`finishedsquarefeet6`,
-    p.`fips`,
-    svi.`COUNTY` county,
-    svi.`ST_ABBR` state,
-    p.`fireplacecnt`,
-    p.`fullbathcnt`,
-    p.`garagecarcnt`,
-    p.`garagetotalsqft`,
-    p.`hashottuborspa`,
-    p.`heatingorsystemtypeid`,
-    hst.`heatingorsystemdesc`,
+--    p.`decktypeid`,
+--    p.`finishedfloor1squarefeet`,
+--    p.`finishedsquarefeet12`,
+--    p.`finishedsquarefeet13`,
+--    p.`finishedsquarefeet15`,
+--    p.`finishedsquarefeet50`,
+--    p.`finishedsquarefeet6`,
+--    p.`fips`,
+--    svi.`COUNTY` county,
+--    svi.`ST_ABBR` state,
+--    p.`fireplacecnt`,
+--    p.`fullbathcnt`,
+--    p.`garagecarcnt`,
+--    p.`garagetotalsqft`,
+--    p.`hashottuborspa`,
+--    p.`heatingorsystemtypeid`,
+--    hst.`heatingorsystemdesc`,
     p.`latitude`,
     p.`longitude`,
-    p.`lotsizesquarefeet`,
-    p.`poolcnt`,
+--    p.`lotsizesquarefeet`,
+--    p.`poolcnt`,
 --    p.`poolsizesum`,
 --    p.`pooltypeid10`,
 --    p.`pooltypeid2`,
 --    p.`pooltypeid7`,
-    p.`propertycountylandusecode`,
+--    p.`propertycountylandusecode`,
     p.`propertylandusetypeid`,
     plut.`propertylandusedesc`,
-    p.`propertyzoningdesc`,
-    p.`rawcensustractandblock`,
-    p.`regionidcity`,
-    p.`regionidcounty`,
-    p.`regionidneighborhood`,
-    p.`regionidzip`,
-    p.`roomcnt`,
-    p.`storytypeid`,
-    st.`storydesc`,
-    p.`taxvaluedollarcnt`,
-    p.`threequarterbathnbr`,
-    p.`unitcnt`,
-    p.`yardbuildingsqft17`,
-    p.`yardbuildingsqft26`,
-    p.`yearbuilt`,
-    p.`numberofstories`,
-    p.`fireplaceflag`,
-    p.`structuretaxvaluedollarcnt`,
-    p.`assessmentyear`,
-    p.`landtaxvaluedollarcnt`,
-    p.`taxamount`,
-    p.`taxdelinquencyflag`,
-    p.`taxdelinquencyyear`, 
-    p.`typeconstructiontypeid`,
-    tct.`typeconstructiondesc`,
-    p.`censustractandblock`,
-    pred.`transactiondate`,
+--    p.`propertyzoningdesc`,
+--    p.`rawcensustractandblock`,
+--    p.`regionidcity`,
+--    p.`regionidcounty`,
+--    p.`regionidneighborhood`,
+--    p.`regionidzip`,
+--    p.`roomcnt`,
+--    p.`storytypeid`,
+--    st.`storydesc`,
+--    p.`taxvaluedollarcnt`,
+--    p.`threequarterbathnbr`,
+--    p.`unitcnt`,
+--    p.`yardbuildingsqft17`,
+--    p.`yardbuildingsqft26`,
+--    p.`yearbuilt`,
+--    p.`numberofstories`,
+--    p.`fireplaceflag`,
+--    p.`structuretaxvaluedollarcnt`,
+--    p.`assessmentyear`,
+--    p.`landtaxvaluedollarcnt`,
+--    p.`taxamount`,
+--    p.`taxdelinquencyflag`,
+--    p.`taxdelinquencyyear`, 
+--    p.`typeconstructiontypeid`,
+--    tct.`typeconstructiondesc`,
+--    p.`censustractandblock`,
+--    pred.`transactiondate`,
     pred.`logerror`,
-    m.`transactions`,
-    p.`taxamount`/p.`taxvaluedollarcnt` tax_rate
+    m.`transactions`
+--    ,
+--    p.`taxamount`/p.`taxvaluedollarcnt` tax_rate
 from 
     `properties_2017` p
 inner join `predictions_2017`  pred
@@ -102,20 +103,20 @@ inner join
 left join `propertylandusetype` plut
     on p.`propertylandusetypeid` = plut.`propertylandusetypeid`
         
-left join svi_db.svi2016_us_county svi
-    on p.`fips` = svi.`FIPS`
-left join `airconditioningtype` act
-    using(`airconditioningtypeid`)
-left join heatingorsystemtype hst
-    using(`heatingorsystemtypeid`)
-left join `architecturalstyletype` ast
-    using(`architecturalstyletypeid`)
-left join `buildingclasstype` bct
-    using(`buildingclasstypeid`)
-left join `storytype` st
-    using(`storytypeid`)
-left join `typeconstructiontype` tct
-    using(`typeconstructiontypeid`)
+-- left join svi_db.svi2016_us_county svi
+--     on p.`fips` = svi.`FIPS`
+-- left join `airconditioningtype` act
+--     using(`airconditioningtypeid`)
+-- left join heatingorsystemtype hst
+--     using(`heatingorsystemtypeid`)
+-- left join `architecturalstyletype` ast
+--     using(`architecturalstyletypeid`)
+-- left join `buildingclasstype` bct
+--     using(`buildingclasstypeid`)
+-- left join `storytype` st
+--     using(`storytypeid`)
+-- left join `typeconstructiontype` tct
+--     using(`typeconstructiontypeid`)
 
 where 
     p.`latitude` is not null
