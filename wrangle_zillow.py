5323186d456991c92bc00d2aa2c98574998ada6e
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-11-06 10:53:27 -0600

    updated wrangle_zillow.py

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -4,7 +4,125 @@ from acquire import sql_df
 from dfo import DFO
 
 
-sql = '''
+sqls = {
+'full': '''
+select 
+    p.`id`,
+    p.`parcelid`,
+    p.`airconditioningtypeid`,
+    act.`airconditioningdesc`,
+    p.`architecturalstyletypeid`,
+    ast.`architecturalstyledesc`,
+    p.`basementsqft`,
+    p.`bathroomcnt`,
+    p.`bedroomcnt`,
+    p.`buildingclasstypeid`,
+    bct.`buildingclassdesc`,
+    p.`buildingqualitytypeid`,
+    p.`calculatedbathnbr`,
+    p.`calculatedfinishedsquarefeet`,
+    p.`decktypeid`,
+    p.`finishedfloor1squarefeet`,
+    p.`finishedsquarefeet12`,
+    p.`finishedsquarefeet13`,
+    p.`finishedsquarefeet15`,
+    p.`finishedsquarefeet50`,
+    p.`finishedsquarefeet6`,
+    p.`fips`,
+    svi.`COUNTY` county,
+    svi.`ST_ABBR` state,
+    p.`fireplacecnt`,
+    p.`fullbathcnt`,
+    p.`garagecarcnt`,
+    p.`garagetotalsqft`,
+    p.`hashottuborspa`,
+    p.`heatingorsystemtypeid`,
+    hst.`heatingorsystemdesc`,
+    p.`latitude`,
+    p.`longitude`,
+    p.`lotsizesquarefeet`,
+    p.`poolcnt`,
+    p.`poolsizesum`,
+    p.`pooltypeid10`,
+    p.`pooltypeid2`,
+    p.`pooltypeid7`,
+    p.`propertycountylandusecode`,
+    p.`propertylandusetypeid`,
+    plut.`propertylandusedesc`,
+    p.`propertyzoningdesc`,
+    p.`rawcensustractandblock`,
+    p.`regionidcity`,
+    p.`regionidcounty`,
+    p.`regionidneighborhood`,
+    p.`regionidzip`,
+    p.`roomcnt`,
+    p.`storytypeid`,
+    st.`storydesc`,
+    p.`taxvaluedollarcnt`,
+    p.`threequarterbathnbr`,
+    p.`unitcnt`,
+    p.`yardbuildingsqft17`,
+    p.`yardbuildingsqft26`,
+    p.`yearbuilt`,
+    p.`numberofstories`,
+    p.`fireplaceflag`,
+    p.`structuretaxvaluedollarcnt`,
+    p.`assessmentyear`,
+    p.`landtaxvaluedollarcnt`,
+    p.`taxamount`,
+    p.`taxdelinquencyflag`,
+    p.`taxdelinquencyyear`, 
+    p.`typeconstructiontypeid`,
+    tct.`typeconstructiondesc`,
+    p.`censustractandblock`,
+    pred.`transactiondate`,
+    pred.`logerror`,
+    m.`transactions`,
+   p.`taxamount`/p.`taxvaluedollarcnt` tax_rate
+from 
+    `properties_2017` p
+inner join `predictions_2017`  pred
+    on p.`parcelid` = pred.`parcelid` 
+inner join 
+    (select 
+        `parcelid`, 
+        max(`transactiondate`) `lasttransactiondate`, 
+        max(`id`) `maxid`, 
+        count(*) `transactions`
+    from 
+        predictions_2017
+    where
+        transactiondate like '2017%%'
+    group by 
+        `parcelid`
+    ) m
+    on 
+    pred.parcelid = m.parcelid
+    and pred.transactiondate = m.lasttransactiondate
+
+left join `propertylandusetype` plut
+    on p.`propertylandusetypeid` = plut.`propertylandusetypeid`
+        
+left join svi_db.svi2016_us_county svi
+    on p.`fips` = svi.`FIPS`
+left join `airconditioningtype` act
+    using(`airconditioningtypeid`)
+left join heatingorsystemtype hst
+    using(`heatingorsystemtypeid`)
+left join `architecturalstyletype` ast
+    using(`architecturalstyletypeid`)
+left join `buildingclasstype` bct
+    using(`buildingclasstypeid`)
+left join `storytype` st
+    using(`storytypeid`)
+left join `typeconstructiontype` tct
+    using(`typeconstructiontypeid`)
+
+where 
+    p.`latitude` is not null
+    and p.`longitude` is not null;
+''', 
+'mvp': '''
 select 
 --    p.`id`,
     p.`parcelid`,
@@ -122,10 +240,11 @@ where
     p.`latitude` is not null
     and p.`longitude` is not null;
 '''
+}
 
 
 @timeifdebug
-def get_zillow_data(sql=sql, splain=local_settings.splain, **kwargs):
+def get_zillow_data(sql=sqls['full'], splain=local_settings.splain, **kwargs):
     '''
     get_zillow_data(splain=local_settings.splain, **kwargs)
     RETURNS: dataframe
@@ -141,3 +260,4 @@ def prep_zillow_data(dfo=None, df=None, splain=local_settings.splain, **kwargs):
     if df is None:
         df = dfo.df
     df = convert_to_dates(df, cols=['transactiondate'])
+
